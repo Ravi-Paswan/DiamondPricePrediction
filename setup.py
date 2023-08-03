650175025992ad3a64deca8b175d4d19c2c70e25
Ravi-Paswan <ravipaswanatmotorola@gmai.com>
2023-08-03 12:06:38 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+HYPEN_E_DOT = '-e .'
+
+
+def get_requirments(file_path:str)->List[str]:
+    requirments = []
+    with open(file_path) as file_obj:
+        requirments = file_obj.readlines()
+        requirments = [req.replace("\n","") for req in requirments]
+        if HYPEN_E_DOT in requirments:
+            requirments.remove(HYPEN_E_DOT)
+        return requirments
+
+
+
+setup(
+    name = 'DiamondPricePrediction',
+    version = '0.0.1',
+    author = 'Ravi',
+    author_email = "ravipaswanatmmotorola@gamil.com",
+    install_requires = get_requirments('requirments.txt'),
+    packages = find_packages()  
+)
